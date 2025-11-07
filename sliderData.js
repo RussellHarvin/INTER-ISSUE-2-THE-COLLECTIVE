c39b0b03338b777ff9bed25f5b5a7cfea83338b5
RussellHarvin <harvin2@g.clemson.edu>
2025-11-07 11:49:32 -0500

    Add files via upload

---
+++ sliderData.js
@@ -0,0 +1,57 @@
+export const sliderData = [
+    {
+    title: "EIGHT",
+    img: "ARCHIMG/slider_img_11.jpg",
+    url: "/Work.html",
+  },
+  {
+    title: "FIRST",
+    img: "ARCHIMG/slider_img_01.jpg",
+    url: "/Work.html",
+  },
+  {
+    title: "SECOND",
+    img: "ARCHIMG/slider_img_02.jpg",
+    url: "/Work.html",
+  },
+  {
+    title: "THIRD",
+    img: "ARCHIMG/slider_img_03.jpg",
+    url: "/Work.html",
+  },
+  {
+    title: "FOURTH",
+    img: "ARCHIMG/slider_img_04.jpg",
+    url: "/Work.html",
+  },
+  {
+    title: "FIFTH",
+    img: "ARCHIMG/slider_img_05.jpg",
+    url: "/Work.html",
+  },
+  {
+    title: "SIX",
+    img: "ARCHIMG/slider_img_06.jpg",
+    url: "/Work.html",
+  },
+  {
+    title: "SEVEN",
+    img: "ARCHIMG/slider_img_07.jpg",
+    url: "/Work.html",
+  },
+  {
+    title: "EIGHT",
+    img: "ARCHIMG/slider_img_08.jpg",
+    url: "/Work.html",
+  },
+    {
+    title: "EIGHT",
+    img: "ARCHIMG/slider_img_09.jpg",
+    url: "/Work.html",
+  },
+    {
+    title: "EIGHT",
+    img: "ARCHIMG/slider_img_10.jpg",
+    url: "/Work.html",
+  },
+];
