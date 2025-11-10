5040cca3a23d92ff0e71f55192ff1356a3ff5389
RussellHarvin <harvin2@g.clemson.edu>
2025-11-09 21:35:25 -0500

    Add files via upload

--- sliderData.js
+++ sliderData.js
@@ -2,56 +2,56 @@ export const sliderData = [
     {
     title: "FIRST",
     img: "slider_img_11_medium.jpeg",
-    url: "/Work.html",
+    url: "Work.html",
   },
   {
     title: "SECOND",
     img: "slider_img_01_medium.jpeg",
-    url: "/Work.html",
+    url: "Work.html",
   },
   {
     title: "THIRD",
     img: "slider_img_02_medium.jpeg",
-    url: "/Work.html",
+    url: "Work.html",
   },
   {
     title: "FORUTH",
     img: "slider_img_03_medium.jpeg",
-    url: "/Work.html",
+    url: "Work.html",
   },
   {
     title: "FIFTH",
     img: "slider_img_04_medium.jpeg",
-    url: "/Work.html",
+    url: "Work.html",
   },
   {
     title: "SIXTH",
     img: "slider_img_05_medium.jpeg",
-    url: "/Work.html",
+    url: "Work.html",
   },
   {
     title: "SEVEN",
     img: "slider_img_06_medium.jpeg",
-    url: "/Work.html",
+    url: "Work.html",
   },
   {
     title: "EIGHT",
     img: "slider_img_07_medium.jpeg",
-    url: "/Work.html",
+    url: "Work.html",
   },
   {
     title: "NINE",
     img: "slider_img_08_medium.jpeg",
-    url: "/Work.html",
+    url: "Work.html",
   },
     {
     title: "TEN",
     img: "slider_img_09_medium.jpeg",
-    url: "/Work.html",
+    url: "Work.html",
   },
     {
     title: "ELEVEN",
     img: "slider_img_10_medium.jpeg",
-    url: "/Work.html",
+    url: "Work.html",
   },
 ];
